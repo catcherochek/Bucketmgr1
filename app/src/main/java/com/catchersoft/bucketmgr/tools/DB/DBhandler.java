a993b34ba0513d8512fbfd84df1cf5f64bd64c8c
catchersoft <catcherochek@yandex.ru>
2018-04-07 19:56:09 +0300

    класс DBHandler, который занимается выборками к бд

---
+++ app/src/main/java/com/catchersoft/bucketmgr/tools/DB/DBhandler.java
@@ -0,0 +1,17 @@
+package com.catchersoft.bucketmgr.tools.DB;
+
+import android.content.ContentResolver;
+import android.database.CharArrayBuffer;
+import android.database.ContentObserver;
+import android.database.Cursor;
+import android.database.DataSetObserver;
+import android.net.Uri;
+import android.os.Bundle;
+
+/**
+ * Created by Клим on 06.04.2018.
+ */
+
+public class DBhandler {
+
+}
