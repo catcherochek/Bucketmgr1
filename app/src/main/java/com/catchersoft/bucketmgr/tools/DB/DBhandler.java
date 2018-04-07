9087130c03c46eaaae2b7f445ef0c718b68a8490
catchersoft <catcherochek@yandex.ru>
2018-04-07 20:24:28 +0300

    класс DBHandler, который занимается выборками к бд

--- app/src/main/java/com/catchersoft/bucketmgr/tools/DB/DBhandler.java
+++ app/src/main/java/com/catchersoft/bucketmgr/tools/DB/DBhandler.java
@@ -1,17 +1,49 @@
 package com.catchersoft.bucketmgr.tools.DB;
 
-import android.content.ContentResolver;
-import android.database.CharArrayBuffer;
-import android.database.ContentObserver;
+import android.content.Context;
 import android.database.Cursor;
-import android.database.DataSetObserver;
-import android.net.Uri;
-import android.os.Bundle;
+import android.database.sqlite.SQLiteDatabase;
 
 /**
- * Created by Клим on 06.04.2018.
+ * Created by Клим on 07.04.2018.
  */
 
-public class DBhandler {
+class DBHandler {
+    private static DBHandler ourInstance;
+    private static DBhelper dbh;
+    private static Context context;
+    static DBHandler getInstance(Context  contex) {
+        if (ourInstance == null)
+        {
+            ourInstance= new DBHandler();
+            dbh = DBhelper.getInstance(contex);
+            context=contex;
+        }
+        return ourInstance;
+    }
+
+    private DBHandler() {
+
+    }
+
+
+    public Cursor InitRead(String query){
+        if ((ourInstance != null) & (dbh != null)) {
+            SQLiteDatabase sd = dbh.getWritableDatabase();
+            Cursor c = sd.rawQuery(query,new String[]{});
+            if (c != null) {
+                return  c;
+            }
+        }
+        return null;
+    }
+    public void InitWrite(String query){
+        if ((ourInstance != null) & (dbh != null)) {
+            SQLiteDatabase sd = dbh.getWritableDatabase();
+            sd.execSQL(query);
+        }
+
+
+    }
 
 }
